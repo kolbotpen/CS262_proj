e196766164f37a9f4c5861a2ee70e41e9f8e8795
Chansovisoth Wattanak <cwattanak@paragoniu.edu.kh>
2024-05-31 10:09:16 +0700

    Edited the table headers and icons

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -14,7 +14,7 @@
                     <th class="align-middle"></th>
                     <th class="align-middle text-center">
                         <button class="btn button-gray d-inline-flex align-items-center">
-                            <img class="icon me-2" src="assets/images/icon-user-add.svg" draggable="false">Add Member
+                            <img class="icon me-2" src="assets/images/icon-team.svg" draggable="false">Add Team
                         </button>
                     </th>
                 </tr>
@@ -23,36 +23,36 @@
         <table class="table m-0" style="table-layout: fixed; width: 100%;">
             <thead>
                 <tr>
-                    <th class="align-middle">Team Members</th>
-                    <th class="align-middle">Titles</th>
+                    <th class="align-middle">No.</th>
+                    <th class="align-middle">Team</th>
                     <th class="align-middle text-center w-16">Options</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td class="align-middle">Visoth</td>
+                    <td class="align-middle">1</td>
                     <td class="align-middle">Front-end Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">Kolbot</td>
+                    <td class="align-middle">2</td>
                     <td class="align-middle">API Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">Soponloe</td>
+                    <td class="align-middle">3</td>
                     <td class="align-middle">Back-end Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
@@ -71,7 +71,7 @@
                     <th class="align-middle"></th>
                     <th class="align-middle text-center">
                         <button class="btn button-gray d-inline-flex align-items-center">
-                            <img class="icon me-2" src="assets/images/icon-user-add.svg" draggable="false">Add Member
+                            <img class="icon me-2" src="assets/images/icon-team.svg" draggable="false">Add Team
                         </button>
                     </th>
                 </tr>
@@ -80,36 +80,36 @@
         <table class="table m-0" style="table-layout: fixed; width: 100%;">
             <thead>
                 <tr>
-                    <th class="align-middle">Team Members</th>
-                    <th class="align-middle">Titles</th>
+                    <th class="align-middle">No.</th>
+                    <th class="align-middle">Team</th>
                     <th class="align-middle text-center w-16">Options</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td class="align-middle">Adjective Vitou</td>
+                    <td class="align-middle">1</td>
                     <td class="align-middle">Front-end Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">John Xina</td>
+                    <td class="align-middle">2</td>
                     <td class="align-middle">API Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">Lebron James</td>
+                    <td class="align-middle">3</td>
                     <td class="align-middle">Back-end Developer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
@@ -128,7 +128,7 @@
                     <th class="align-middle"></th>
                     <th class="align-middle text-center">
                         <button class="btn button-gray d-inline-flex align-items-center">
-                            <img class="icon me-2" src="assets/images/icon-user-add.svg" draggable="false">Add Member
+                            <img class="icon me-2" src="assets/images/icon-team.svg" draggable="false">Add Team
                         </button>
                     </th>
                 </tr>
@@ -137,36 +137,36 @@
         <table class="table m-0" style="table-layout: fixed; width: 100%;">
             <thead>
                 <tr>
-                    <th class="align-middle">Team Members</th>
-                    <th class="align-middle">Titles</th>
+                    <th class="align-middle">No.</th>
+                    <th class="align-middle">Team</th>
                     <th class="align-middle text-center w-16">Options</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td class="align-middle">Robert House</td>
+                    <td class="align-middle">1</td>
                     <td class="align-middle">Lead Engineer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">Tim Cain</td>
+                    <td class="align-middle">2</td>
                     <td class="align-middle">Overseer</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
                 <tr>
-                    <td class="align-middle">Cooper Howard</td>
+                    <td class="align-middle">3</td>
                     <td class="align-middle">Advertising</td>
                     <td class="align-middle text-center">
                         <button class="btn btn-primary">
-                            <img class="icon" src="assets/images/icon-mail.svg" draggable="false">Message
+                            <img class="icon" src="assets/images/icon-view.svg" draggable="false">View Team
                         </button>
                     </td>
                 </tr>
