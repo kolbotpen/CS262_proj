f38c0934ee191feb63dc383dd8ff81b58274bec2
Soponloe <sovannsoponloe@gmail.com>
2024-06-15 15:58:30 +0700

    Update user-workspace.blade.php

--- resources/views/admin/user-workspace.blade.php
+++ resources/views/admin/user-workspace.blade.php
@@ -38,6 +38,7 @@
 							<th width="25">#</th>
 							<th width="375">User</th>
 							<th width="300">Email</th>
+							<th width="300">Team</th>
 							<th width="300">Status</th>
 							<th width="100">Option</th>
 						</tr>
@@ -45,24 +46,35 @@
 					<tbody>
 						@foreach ($users as $index => $user)
 							<tr>
-								<td>{{$index+1}}</td>
+								<td>{{$index + 1}}</td>
 								<td>{{ $user->name }}</td>
 								<td>{{$user->email}}</td>
+								<td>
+									@foreach ($user->teams as $team)
+										{{ $team->name }} <!-- Display the team name -->
+									@endforeach
+								</td>
 								<td>Active</td>
 								<td>
 									<a href="edit">
 										<svg class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"
 											viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-											<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+											<path
+												d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
+											</path>
 										</svg>
 									</a>
-									<form action="{{ route('users.destroy', $user) }}" method="POST" style="display:inline;">
+									<form action="{{ route('users.destroy', $user) }}" method="POST"
+										style="display:inline;">
 										@csrf
 										@method('DELETE')
-										<button type="submit" class="btn btn-link text-danger p-0" onclick="return confirm('Are you sure you want to delete this user?');">
+										<button type="submit" class="btn btn-link text-danger p-0"
+											onclick="return confirm('Are you sure you want to delete this user?');">
 											<svg class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"
 												viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-												<path ath fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+												<path ath fill-rule="evenodd"
+													d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+													clip-rule="evenodd"></path>
 											</svg>
 										</button>
 									</form>
@@ -84,4 +96,4 @@
 		</div>
 	</div>
 	<!-- /.card -->
-@stop
+	@stop
