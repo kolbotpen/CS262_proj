bbc3011d2e60a58b5b94f4b377c4a871cce453d5
Soponloe <sovannsoponloe@gmail.com>
2024-06-17 16:11:29 +0700

    When you delete a company the team still wont be deleted but it will say NO COMPANY
    IDK WHY IT WOULDNT WORK ITS SPOSE TO WORK WITH CASCADE DELETE

--- database/migrations/2024_06_09_131126_create_teams_table.php
+++ database/migrations/2024_06_09_131126_create_teams_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('company_id')->constrained();
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
